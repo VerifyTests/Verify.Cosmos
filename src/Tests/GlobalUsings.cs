b4f6bcba3618faab81cf4899a6025b62854ba6a4
Simon Cropp <simon.cropp@gmail.com>
2024-09-15 20:24:17 +1000

    refs and cleanup

--- src/Tests/GlobalUsings.cs
+++ src/Tests/GlobalUsings.cs
@@ -1,5 +1,3 @@
-﻿// Global using directives
-
-global using Microsoft.Azure.Cosmos;
+﻿global using Microsoft.Azure.Cosmos;
 global using Microsoft.Azure.Cosmos.Linq;
 global using Newtonsoft.Json;
--- src/Verify.Cosmos/GlobalUsings.cs
+++ src/Verify.Cosmos/GlobalUsings.cs
@@ -1,3 +1,1 @@
-﻿// Global using directives
-
-global using Microsoft.Azure.Cosmos;
+﻿global using Microsoft.Azure.Cosmos;
